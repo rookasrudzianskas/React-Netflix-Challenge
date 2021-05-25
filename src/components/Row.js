f1eb7a69c0c7f26900437a2b069374c73ed2cdcb
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 14:38:27 +0300

    Rokas

--- src/components/Row.js
+++ src/components/Row.js
@@ -26,8 +26,9 @@ function Row({ title, fetchUrl, isLargeRow }) {
             <h2>{title}</h2>
             <div className="row__posters">
                 {movies.map(movie => (
+                    ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
                     <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie?.name}/>
-                ))}
+                    )))}
             </div>
         </div>
     );
