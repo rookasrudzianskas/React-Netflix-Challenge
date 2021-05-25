ae3300a2576beda36c8b4483260df90e3ed6c7e5
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 11:10:25 +0300

    Rokas

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import "./styles/Nav.css";
 
 const Nav = () => {
     return (
-        <div className="nav">
+        <div className="nav nav__black">
             <div className="nav__contents">
                 <img className="nav__logo" src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png" alt=""/>
 
