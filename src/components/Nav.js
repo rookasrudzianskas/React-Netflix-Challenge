44ea232af4d3face9a117547b8209b69c0017e12
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 11:18:18 +0300

    Rokas

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Nav.css";
 
 const Nav = () => {
+
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if(window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavBar);
+
+        return () => window.removeEventListener("scroll", transitionNavBar);
+    }, []);
+
     return (
-        <div className="nav nav__black">
+        <div className={`nav ${show && 'nav__black'}`}>
             <div className="nav__contents">
                 <img className="nav__logo" src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png" alt=""/>
 
