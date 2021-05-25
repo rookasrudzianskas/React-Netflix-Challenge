bc8a58793a00870c7fa02fad61a8c3a52ee95c5f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 13:46:08 +0300

    Roaks

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Banner.css";
+import axios from "../axios";
+import requests from "../Requests";
 
 const Banner = () => {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            // makes  a request
+            // for original ones
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                /// and the sets, from request data and results, the one, which is corresponding to the random number!
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                    ]
+            );
+            return request;
+        }
+        fetchData();
+
+    }, []);
+
+    console.log(movie)
 
     const truncate = (string, n) => {
         return string?.length > n  ? string.substring(0, n -1) + "...." : string;
@@ -9,7 +32,7 @@ const Banner = () => {
     return (
         <header style={{
             backgroundSize: 'cover',
-            backgroundImage: `url(https://videotapenews.com/wp-content/uploads/2020/07/avatar-the-last-airbender-netflix-show.jpg)`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: 'center center',
         }} className="banner">
 
