ccc133725b7857c624ba8bf86a7e5c11f73dd4d1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 14:58:33 +0300

    Rokas

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
 import "./styles/Row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original"
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
 
     useEffect(() => {
 
@@ -18,6 +22,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    // netlfix trailer details
+    const opts = {
+        height: "390",
+        width: "99%",
+        playerVars: {
+            autoplay: 0,
+        }
+    }
+
+    const handleClick = (movie) => {
+        // console.table(movie?.title)
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.title || "")
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                }).catch((error) => console.log(error));
+        }
+    }
+
 
 
 
@@ -27,9 +53,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
             <div className="row__posters">
                 {movies.map(movie => (
                     ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
-                    <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie?.name}/>
+                    <img key={movie.id} onClick={() => handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie?.name}/>
                     )))}
             </div>
+
+            <div style={{ padding: "40px" }}>
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+            </div>
+
         </div>
     );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5Q9X3vwzFeQvgG4awhTF-NPhlQzFBTkE",
+    authDomain: "rookas-netflix-app.firebaseapp.com",
+    projectId: "rookas-netflix-app",
+    storageBucket: "rookas-netflix-app.appspot.com",
+    messagingSenderId: "163774538371",
+    appId: "1:163774538371:web:cf58f132d297195e4171b6",
+    measurementId: "G-0RFJYTSE1L"
+};
+
