44cc2d0640b88fb353ee2d267d931fae7d1d9a11
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 10:53:08 +0300

    Rokas

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/Nav.css";
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            NAV
+        </div>
+    );
+};
+
+export default Nav;
