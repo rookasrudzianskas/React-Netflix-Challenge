b0f465c0e591f8fc090f3d388e3935503978690d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-25 11:29:37 +0300

    Rokas

---
+++ src/components/Banner.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./styles/Banner.css";
+
+const Banner = () => {
+    return (
+        <header style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(https://videotapenews.com/wp-content/uploads/2020/07/avatar-the-last-airbender-netflix-show.jpg)`,
+            backgroundPosition: 'center center',
+        }} className="banner">
+
+
+            <div className="banner__contents">
+                <h1 className="bannerTitle">
+                    Movie Name
+                </h1>
+
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+
+                <h1 className="banner__description">
+                    This is a test description
+                </h1>
+            </div>
+
+            <div className="banner--fadeBottom" />
+
+        </header>
+    );
+};
+
+export default Banner;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,66 +1,14 @@
 import React from 'react';
 import "./styles/HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 const HomeScreen = () => {
     return (
         <div className="homeScreen">
             <Nav />
+                <Banner />
 
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
-            <h1>TEST</h1>
         </div>
     );
 };
