aa40fca0c7337643d84984afd583ea91d7778dd7
Rokas <rookas.rudzenskas@gmail.com>
2021-05-26 12:37:07 +0300

    Rokas

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export {auth}
+export {auth};
 export default db;
 
